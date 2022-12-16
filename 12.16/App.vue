92782d93939741df4e3f5803c268dbb72c02a028
xxxx <24736743@qq.com>
2022-12-16 21:11:47 +0800

    周璇-2001B

---
+++ 12.16/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <router-view class="page" />
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  box-sizing: border-box;
+}
+
+body,
+html,
+#app,
+.page {
+  width: 100%;
+  height: 100%;
+}
+.el-container{
+	width: 100%;
+	height: 100%;
+	padding: 0;
+}
+.el-aside{
+	height: 100%;
+	background:#333744 ;
+	color: #fff;
+}
+.el-main{
+	padding: 0 !important;
+	margin: 0;
+	width: 100%;
+	height: 100%;
+
+}
+.el-menu{
+	border: 0 !important;
+}
+
+</style>
