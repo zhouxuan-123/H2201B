073f18a1ece1c3af74ca94a3cda627eedae16abf
xxxx <24736743@qq.com>
2022-12-23 20:40:12 +0800

    周璇-H2201B

---
+++ 12.23/HomeView.vue
@@ -0,0 +1,663 @@
+<template>
+  <div class="home">
+    <h2>联心菜市场</h2>
+    <div class="caBox">
+      <div class="daBox">
+        <div class="aBox">
+          <div class="biaoOne">
+						<img src="../assets/title_left_bg.png" class="img1">
+						<img src="../assets/border_bg.jpg" class="img2">
+						<img src="../assets/border_bg.jpg" class="img3">
+						<img src="../assets/title_right_bg.png" class="img4">
+            <h3>这里是标题1</h3>
+            <p>6890.69<span>元</span></p>
+            <span>小标题：文字 小标题：文字</span>
+          </div>
+          <div class="biaoOne">
+						<img src="../assets/title_left_bg.png" class="img1">
+						<img src="../assets/border_bg.jpg" class="img2">
+						<img src="../assets/border_bg.jpg" class="img3">
+						<img src="../assets/title_right_bg.png" class="img4">
+            <h3>这里是标题2</h3>
+            <p>6090.99<span>元</span></p>
+            <span>小标题：文字 小标题：文字</span>
+          </div>
+          <div class="biaoOne">
+						<img src="../assets/title_left_bg.png" class="img1">
+						<img src="../assets/border_bg.jpg" class="img2">
+						<img src="../assets/border_bg.jpg" class="img3">
+						<img src="../assets/title_right_bg.png" class="img4">
+            <h3>这里是标题3</h3>
+            <p>2890.39<span>元</span></p>
+            <span>小标题：文字 小标题：文字</span>
+          </div>
+          <div class="biaoOne">
+						<img src="../assets/title_left_bg.png" class="img1">
+						<img src="../assets/border_bg.jpg" class="img2">
+						<img src="../assets/border_bg.jpg" class="img3">
+						<img src="../assets/title_right_bg.png" class="img4">
+            <h3>这里是标题4</h3>
+            <p>7590.15<span>元</span></p>
+            <span>小标题：文字 小标题：文字</span>
+          </div>
+        </div>
+        <div class="bBox">
+          <div id="main">
+						<img src="../assets/title_left_bg.png" class="img1">
+						<img src="../assets/border_bg.jpg" class="img2">
+						<img src="../assets/border_bg.jpg" class="img3">
+						<img src="../assets/title_right_bg.png" class="img4">
+					</div>
+
+          <div id="main1">
+						<img src="../assets/title_left_bg.png" class="img1">
+						<img src="../assets/border_bg.jpg" class="img2">
+						<img src="../assets/border_bg.jpg" class="img3">
+						<img src="../assets/title_right_bg.png" class="img4">
+						<h3>这里是标题</h3>
+						<div class="lBox">
+							<div class="q1"></div>
+							<span>第一名</span>
+							<div class="fenBox"></div>
+							<div class="touBox"></div>
+							<span>98.5%</span>
+						</div>
+						<div class="llBox">
+							<div class="qq1"></div>
+							<span>第二名</span>
+							<div class="fennBox"></div>
+							<div class="touuBox"></div>
+							<span>88.5%</span>
+						</div>
+						<div class="lllBox">
+							<div class="qqq1"></div>
+							<span>第三名</span>
+							<div class="fennnBox"></div>
+							<div class="touuuBox"></div>
+							<span>68.5%</span>
+						</div>
+						<div class="lllnBox">
+							<div class="qqq11"></div>
+							<span>第四名</span>
+							<div class="fennneBox"></div>
+							<div class="touuueBox"></div>
+							<span>40.5%</span>
+						</div>
+						<div class="lllnnBox">
+							<div class="qqq111"></div>
+							<span>第五名</span>
+							<div class="fennneeBox"></div>
+							<div class="touuueeBox"></div>
+							<span>22.5%</span>
+						</div>
+					</div>
+        </div>
+        <div class="cBox">
+          <div id="main2"></div>
+          <div class="dBox">
+						<img src="../assets/title_left_bg.png" class="img1">
+						<img src="../assets/border_bg.jpg" class="img2">
+						<img src="../assets/border_bg.jpg" class="img3">
+						<img src="../assets/title_right_bg.png" class="img4">
+            <h3>这里是标题</h3>
+            <div class="dfBox">
+              <div class="kBox">
+                <h1>23</h1>
+                <span>今日收益比例</span>
+              </div>
+              <div class="kBox">
+                <h1>107</h1>
+                <span>本月收益比例</span>
+              </div>
+              <div class="kBox">
+                <h1>107</h1>
+                <span>历史收益比例</span>
+              </div>
+              <div class="kBox">
+                <h1>23</h1>
+                <span>今日收益比例</span>
+              </div>
+              <div class="kBox">
+                <h1>23</h1>
+                <span>本月收益比例</span>
+              </div>
+              <div class="kBox">
+                <h1>23</h1>
+                <span>历史收益比例</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="rightBox">
+        <div id="main3"></div>
+        <div class="topBox">
+					<img src="../assets/title_left_bg.png" class="img1">
+					<img src="../assets/border_bg.jpg" class="img2">
+					<img src="../assets/border_bg.jpg" class="img3">
+					<img src="../assets/title_right_bg.png" class="img4">
+          <h3>这里是标题</h3>
+					<ul>
+						<li>用户</li>
+						<li>类型号</li>
+						<li>类别一</li>
+						<li>类别二</li>
+						<li>类别三</li>
+					</ul>
+					<ul>
+						<li>张三</li>
+						<li>13</li>
+						<li>类别一</li>
+						<li>类别二</li>
+						<li>类别三</li>
+					</ul>
+					<ul>
+						<li>张三</li>
+						<li>14</li>
+						<li>类别一</li>
+						<li>类别二</li>
+						<li>类别三</li>
+					</ul>
+					<ul>
+						<li>张三</li>
+						<li>15</li>
+						<li>类别一</li>
+						<li>类别二</li>
+						<li>类别三</li>
+					</ul>
+					<ul>
+						<li>张三</li>
+						<li>16</li>
+						<li>类别一</li>
+						<li>类别二</li>
+						<li>类别三</li>
+					</ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    var chartDom = document.getElementById('main')
+    var myChart = echarts.init(chartDom)
+    var option
+
+    var chartDom2 = document.getElementById('main2')
+    var myChart2 = echarts.init(chartDom2)
+    var option2
+
+    var chartDom3 = document.getElementById('main3')
+    var myChart3 = echarts.init(chartDom3)
+    var option3
+
+    option3 = {
+      title: {
+        text: '这里是标题',
+				textStyle: {
+      		color: '#fff'
+    		},
+        subtext: 'Fake Data',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 1048, name: '西凉' },
+            { value: 735, name: '益州' },
+            { value: 580, name: '登州' },
+            { value: 484, name: '晋州' },
+            { value: 300, name: '幽州' }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    }
+
+    option3 && myChart3.setOption(option3)
+
+    option2 = {
+      title: {
+        text: '这里是标题',
+				textStyle: {
+      		color: '#fff'
+    		}
+      },
+      xAxis: {
+        type: 'category',
+        data: ['00:00', '02:00', '04:00', '06:00', '08:00', '10:00', '12:00']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [200, 150, 194, 218, 305, 247, 160],
+          type: 'line',
+          color: '#742C5A'
+        },
+        {
+          data: [120, 80, 116, 88, 145, 117, 105],
+          type: 'line',
+          color: '#09C4E4'
+        }
+      ]
+    }
+
+    option2 && myChart2.setOption(option2)
+
+
+    let dataAxis = [
+      220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133,
+    ]
+    // prettier-ignore
+    let data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133];
+    let yMax = 500
+    let dataShadow = []
+    for (let i = 0; i < data.length; i++) {
+      dataShadow.push(yMax)
+    }
+    option = {
+      title: {
+        text: '这里是标题',
+        subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom',
+				textStyle: {
+      		color: '#fff'
+    		}
+      },
+      xAxis: {
+        data: dataAxis,
+        axisLabel: {
+          inside: true,
+          color: '#fff'
+        },
+        axisTick: {
+          show: false
+        },
+        axisLine: {
+          show: false
+        },
+        z: 10
+      },
+      yAxis: {
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          color: '#999'
+        }
+      },
+      dataZoom: [
+        {
+          type: 'inside'
+        }
+      ],
+      series: [
+        {
+          type: 'bar',
+          showBackground: true,
+          itemStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              { offset: 0, color: '#83bff6' },
+              { offset: 0.5, color: '#188df0' },
+              { offset: 1, color: '#188df0' }
+            ])
+          },
+          emphasis: {
+            itemStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                { offset: 0, color: '#2378f7' },
+                { offset: 0.7, color: '#2378f7' },
+                { offset: 1, color: '#83bff6' }
+              ])
+            }
+          },
+          data: data
+        }
+      ]
+    }
+    // Enable data zoom when user click bar.
+    const zoomSize = 6
+    myChart.on('click', function (params) {
+      console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)])
+      myChart.dispatchAction({
+        type: 'dataZoom',
+        startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
+        endValue:
+          dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
+      })
+    })
+
+    option && myChart.setOption(option)
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  // text-align: center;
+  width: 100%;
+  height: 100%;
+  background-image: url(../assets/bg2.jpg);
+  background-size: 100%;
+}
+.daBox {
+  padding: 20px;
+}
+h2 {
+  margin-left: 690px;
+  padding: 15px;
+  color: #0ae5f3;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+.biaoOne {
+  padding: 15px;
+  width: 200px;
+  height: 100px;
+  border: 1px solid #0259b8;
+  margin-left: 15px;
+	position: relative;
+}
+.img1{
+	width: 60px;
+	height: 8px;
+	position: absolute;
+	left: 0;
+	top: -7px;
+}
+.img2{
+	width: 20px;
+	height: 20px;
+	position: absolute;
+	right: 0;
+	top: 0;
+}
+.img3{
+	width: 20px;
+	height: 20px;
+	position: absolute;
+	left: 0;
+	bottom: 0;
+}
+.img4{
+	width: 20px;
+	height: 20px;
+	position: absolute;
+	right: 0;
+	bottom: 0;
+}
+h3 {
+  color: #fff;
+}
+p {
+  margin-top: 10px;
+  color: #0ae5f3;
+  font-size: 30px;
+}
+span {
+  font-size: 10px;
+  color: #fff;
+}
+.aBox {
+  display: flex;
+}
+#main {
+  padding: 15px;
+  width: 500px;
+  height: 200px;
+  border: 1px solid #0259b8;
+  color: #fff;
+  margin-top: 15px;
+  margin-left: 15px;
+	position: relative;
+}
+#main1 {
+  padding: 13px;
+  border: 1px solid #0259b8;
+  width: 400px;
+  height: 200px;
+  margin-top: 15px;
+  margin-left: 15px;
+	position: relative;
+}
+#main2 {
+  padding: 15px;
+  width: 500px;
+  height: 193px;
+  border: 1px solid #0259b8;
+  color: #fff;
+  margin-top: 15px;
+  margin-left: 15px;
+}
+.dBox {
+  padding: 11px;
+  border: 1px solid #0259b8;
+  width: 400px;
+  height: 200px;
+  margin-top: 15px;
+  margin-left: 15px;
+	position: relative;
+}
+.bBox {
+  display: flex;
+}
+.cBox {
+  display: flex;
+}
+.kBox {
+  margin-top: 20px;
+  text-align: center;
+  margin-left: 45px;
+}
+h1 {
+  color: #0bd5ec;
+}
+.dfBox {
+  display: flex;
+  flex-wrap: wrap;
+}
+.caBox {
+  display: flex;
+}
+#main3 {
+  padding: 15px;
+  border: 1px solid #0259b8;
+  width: 440px;
+  height: 230px;
+  margin-top: 20px;
+  // margin-left: 15px;
+}
+.topBox {
+  padding: 10px;
+  border: 1px solid #0259b8;
+  width: 440px;
+  height: 314px;
+  margin-top: 20px;
+  position: relative;
+}
+ul{
+	display: flex;
+	height: 50px;
+	line-height: 50px;
+	border-bottom: 2px solid #0557AC;
+	li{
+		list-style: none;
+		color: #fff;
+		margin-left: 35px;
+	}
+}
+.q1{
+	width: 15px;
+	height: 15px;
+	border-radius: 50px;
+	background-color: #F78CF3;
+	// margin-left: 10px;
+}
+.lBox{
+	display: flex;
+	position: relative;
+	margin-top: 15px;
+}
+.fenBox{
+	width: 250px;
+	height: 15px;
+	background-color: #F78CF3;
+	border-radius: 50px;
+	margin-left: 65px;
+	position: absolute;
+	left: 15px;
+}
+.touBox{
+	width: 255px;
+	height: 15px;
+	background-color: none;
+	border-radius: 50px;
+	background-color: #8093CF;
+	margin-left: 30px;
+}
+.qq1{
+	width: 15px;
+	height: 15px;
+	border-radius: 50px;
+	background-color: #E7FEB8;
+	// margin-left: 10px;
+}
+.llBox{
+	display: flex;
+	position: relative;
+	margin-top: 15px;
+}
+.fennBox{
+	width: 230px;
+	height: 15px;
+	background-color: #E7FEB8;
+	border-radius: 50px;
+	margin-left: 65px;
+	position: absolute;
+	left: 15px;
+}
+.touuBox{
+	width: 255px;
+	height: 15px;
+	background-color: none;
+	border-radius: 50px;
+	background-color: #8093CF;
+	margin-left: 30px;
+}
+.qqq1{
+	width: 15px;
+	height: 15px;
+	border-radius: 50px;
+	background-color: #FDEA8A;
+	// margin-left: 10px;
+}
+.lllBox{
+	display: flex;
+	position: relative;
+	margin-top: 15px;
+}
+.fennnBox{
+	width: 200px;
+	height: 15px;
+	background-color: #FDEA8A;
+	border-radius: 50px;
+	margin-left: 65px;
+	position: absolute;
+	left: 15px;
+}
+.touuuBox{
+	width: 255px;
+	height: 15px;
+	background-color: none;
+	border-radius: 50px;
+	background-color: #8093CF;
+	margin-left: 30px;
+}
+.qqq11{
+	width: 15px;
+	height: 15px;
+	border-radius: 50px;
+	background-color: #8FF9F8;
+	// margin-left: 10px;
+}
+.lllnBox{
+	display: flex;
+	position: relative;
+	margin-top: 15px;
+}
+.fennneBox{
+	width: 100px;
+	height: 15px;
+	background-color: #8FF9F8;
+	border-radius: 50px;
+	margin-left: 65px;
+	position: absolute;
+	left: 15px;
+}
+.touuueBox{
+	width: 255px;
+	height: 15px;
+	background-color: none;
+	border-radius: 50px;
+	background-color: #8093CF;
+	margin-left: 30px;
+}
+.qqq111{
+	width: 15px;
+	height: 15px;
+	border-radius: 50px;
+	background-color: #D890FA;
+	// margin-left: 10px;
+}
+.lllnnBox{
+	display: flex;
+	position: relative;
+	margin-top: 15px;
+}
+.fennneeBox{
+	width: 50px;
+	height: 15px;
+	background-color: #D890FA;
+	border-radius: 50px;
+	margin-left: 65px;
+	position: absolute;
+	left: 15px;
+}
+.touuueeBox{
+	width: 255px;
+	height: 15px;
+	background-color: none;
+	border-radius: 50px;
+	background-color: #8093CF;
+	margin-left: 30px;
+}
+</style>
