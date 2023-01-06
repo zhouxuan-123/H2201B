2160770c3eda8ca94031ac10fbac04b35b7ad3c7
xxxx <24736743@qq.com>
2023-01-06 22:03:11 +0800

    周璇-H2201B

---
+++ 1.6/social.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="social">
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column label="序号" type="index" width="60" />
+            <el-table-column prop="username" sortable label="姓名" />
+            <el-table-column prop="mobile" sortable label="手机" />
+            <el-table-column prop="workNumber" sortable label="工号" />
+            <el-table-column prop="departmentName" sortable label="部门" />
+            <el-table-column prop="timeOfEntry" sortable label="入职时间" />
+            <el-table-column prop="leaveTime" sortable label="离职时间" />
+            <el-table-column prop="participatingInTheCity" label="社保城市" />
+            <el-table-column prop="providentFundCity" label="公积金城市" />
+            <el-table-column prop="socialSecurityBase" label="社保基数" />
+            <el-table-column prop="providentFundBase" label="公益金基数" />
+        </el-table>
+        <div class="page">
+            <el-pagination background layout="prev, pager, next" :page-size="10" :total="11" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { ref, reactive, toRefs } from 'vue';
+// 引入路由
+const router = useRouter();
+// 引入仓库
+const store = useStore();
+const data = reactive({
+});
+const tableData = [
+    { "id": "1063705482939731968", "username": "cgx", "mobile": "13800000001", "workNumber": "", "departmentName": "", "timeOfEntry": "2018-11-01", "leaveTime": null, "participatingInTheCityId": "1084825908823904256", "participatingInTheCity": "北京", "socialSecurityBase": 10000, "providentFundCityId": "1084825908823904256", "providentFundCity": "北京", "providentFundBase": 10000 },
+    { "id": "1063705989926227968", "username": "666664587", "mobile": "13800000002", "workNumber": "9002", "departmentName": "总裁办", "timeOfEntry": "2018-11-02", "leaveTime": null, "participatingInTheCityId": "1084825908823904256", "participatingInTheCity": "北京", "socialSecurityBase": 10000, "providentFundCityId": "1084825908823904256", "providentFundCity": "北京", "providentFundBase": 10000 },
+    { "id": "1066370498633486336", "username": "孙财", "mobile": "13800000003", "workNumber": "111", "departmentName": "市场部", "timeOfEntry": "2018-11-04", "leaveTime": null, "participatingInTheCityId": "1084825908823904256", "participatingInTheCity": "北京", "socialSecurityBase": 3388, "providentFundCityId": "1084825908823904256", "providentFundCity": "北京", "providentFundBase": 2277 },
+    { "id": "1071632760222810112", "username": "罗晓晓", "mobile": "13800000004", "workNumber": "1111", "departmentName": "人事部", "timeOfEntry": "2018-12-02", "leaveTime": null, "participatingInTheCityId": "1084825908823904256", "participatingInTheCity": "北京", "socialSecurityBase": 5000, "providentFundCityId": "1084825908823904256", "providentFundCity": "北京", "providentFundBase": 5000 },
+    { "id": "1074238801330704384", "username": "文吉星", "mobile": "13400000001", "workNumber": "1001", "departmentName": "人事部", "timeOfEntry": "2018-01-01", "leaveTime": null, "participatingInTheCityId": "1084825908823904256", "participatingInTheCity": "北京", "socialSecurityBase": 5900, "providentFundCityId": "1084825908823904256", "providentFundCity": "北京", "providentFundBase": 5900 },
+    { "id": "1074238801402007552", "username": "巴思慧", "mobile": "13400000002", "workNumber": "1002", "departmentName": "人事部", "timeOfEntry": "2018-01-01", "leaveTime": null, "participatingInTheCityId": null, "participatingInTheCity": null, "socialSecurityBase": null, "providentFundCityId": null, "providentFundCity": null, "providentFundBase": null },
+    { "id": "1075383133106425856", "username": "乔海", "mobile": "13500000001", "workNumber": "2001", "departmentName": "总裁办", "timeOfEntry": "2018-01-01", "leaveTime": null, "participatingInTheCityId": null, "participatingInTheCity": null, "socialSecurityBase": null, "providentFundCityId": null, "providentFundCity": null, "providentFundBase": null },
+    { "id": "1075383135371350016", "username": "董昊空", "mobile": "13500000002", "workNumber": "2002", "departmentName": "总裁办", "timeOfEntry": "2018-01-01", "leaveTime": null, "participatingInTheCityId": null, "participatingInTheCity": null, "socialSecurityBase": null, "providentFundCityId": null, "providentFundCity": null, "providentFundBase": null },
+    { "id": "1075383135459430400", "username": "周乐天", "mobile": "13500000003", "workNumber": "2003", "departmentName": "财务部", "timeOfEntry": "2018-01-01", "leaveTime": null, "participatingInTheCityId": null, "participatingInTheCity": null, "socialSecurityBase": null, "providentFundCityId": null, "providentFundCity": null, "providentFundBase": null },
+    { "id": "1235396724497268736", "username": "吕勇锐", "mobile": "13600000001", "workNumber": "0001", "departmentName": "总裁办", "timeOfEntry": "1992-08-04", "leaveTime": null, "participatingInTheCityId": null, "participatingInTheCity": null, "socialSecurityBase": null, "providentFundCityId": null, "providentFundCity": null, "providentFundBase": null }
+];
+</script>
+
+<style lang="scss" scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.page {
+    margin-top: 10px;
+    float: right;
+}
+</style>
---
+++ 1.6/yearMonth.vue
@@ -0,0 +1,190 @@
+<template>
+<div class='yearBox'>
+    <el-card class="daBox">
+        <button class="dangBox">当前审批中0本月审批通过1本月审批驳回0</button>
+        <el-button type="primary">流程设置</el-button>
+    </el-card>
+    <el-card class="daaBox">
+  <el-table
+    ref="multipleTableRef"
+    :data="tableData"
+    style="width: 100%"
+  >
+  <el-table-column type="selection" width="55" />
+    <el-table-column type="index" label="序号" width="120" />
+    <el-table-column property="processName" sortable label="审批类型" width="120" />
+    <el-table-column property="username" sortable label="申请人" width="120" />
+    <el-table-column property="procCurrNodeUserName" sortable label="当前审批人" width="240" />
+    <el-table-column property="procApplyTime" sortable label="审批发起时间" width="180" />
+    <el-table-column property="processState" sortable label="审批状态" width="120" />
+    <el-table-column fixed="right" label="操作" width="200">
+      <template #default>
+        <el-button link type="primary" size="small">查看</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+    </el-card>
+  <el-pagination
+    small
+    background
+    layout="prev, pager, next"
+    :total="50"
+    class="mt-4"
+  />
+</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+// interface User {
+//   date: string
+//   name: string
+//   address: string
+// }
+
+// const multipleTableRef = ref<InstanceType<typeof ElTable>>()
+// const multipleSelection = ref<User[]>([])
+const toggleSelection = (rows?: User[]) => {
+  if (rows) {
+    rows.forEach((row) => {
+      // TODO: improvement typing when refactor table
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      multipleTableRef.value!.toggleRowSelection(row, undefined)
+    })
+  } else {
+    multipleTableRef.value!.clearSelection()
+  }
+}
+
+const tableData: User[] = [
+{
+				"departmentId":"1175311466846683136",
+				"departmentName":"市场部",
+				"procApplyTime":1569236187000,
+				"procCurrNodeUserId":"107163276022281011210742388013307043841074238801402007552",
+				"procCurrNodeUserName":"文吉星 巴思慧 ",
+				"procData":"{\"duration\":\"1\",\"reason\":\"个人原因\",\"holidayType\":\"1\",\"processName\":\"请假\",\"processKey\":\"process_leave\",\"startTime\":\"2019-09-16 00:00:00\",\"applyUnit\":\"按天\",\"endTime\":\"2019-09-17 00:00:00\",\"userId\":\"1066370498633486336\"}",
+				"processId":"1175967133311934464",
+				"processKey":"process_leave",
+				"processName":"请假",
+				"processState":"2",
+				"timeOfEntry":1541318400000,
+				"userId":"1066370498633486336",
+				"username":"孙财"
+			},
+			{
+				"departmentId":"1175311466846683136",
+				"departmentName":"市场部",
+				"procApplyTime":1569236417000,
+				"procCurrNodeUserId":"10753831331064258561075383135371350016",
+				"procCurrNodeUserName":"乔海 董昊空",
+				"procData":"{\"duration\":\"5\",\"reason\":\"没有原因\",\"holidayType\":\"1\",\"processName\":\"请假\",\"processKey\":\"process_leave\",\"startTime\":\"2019-09-15 00:00:00\",\"applyUnit\":\"按天\",\"endTime\":\"2019-09-19 00:00:00\",\"userId\":\"1066370498633486336\"}",
+				"processId":"1175968095611101184",
+				"processKey":"process_leave",
+				"processName":"请假",
+				"processState":"2",
+				"timeOfEntry":1541318400000,
+				"userId":"1066370498633486336",
+				"username":"孙财"
+			},
+			{
+				"departmentId":"1175311466846683136",
+				"departmentName":"市场部",
+				"procApplyTime":1569236544000,
+				"procCurrNodeUserId":"1063705989926227968",
+				"procCurrNodeUserName":"董昊空",
+				"procData":"{\"duration\":\"1\",\"reason\":\"xxxxx\",\"holidayType\":\"1\",\"processName\":\"请假\",\"processKey\":\"process_leave\",\"startTime\":\"2019-09-09 00:00:00\",\"applyUnit\":\"按天\",\"endTime\":\"2019-09-10 00:00:00\",\"userId\":\"1066370498633486336\"}",
+				"processId":"1175968628937826304",
+				"processKey":"process_leave",
+				"processName":"请假",
+				"processState":"4",
+				"timeOfEntry":1541318400000,
+				"userId":"1066370498633486336",
+				"username":"孙财"
+			},
+			{
+				"departmentId":"1175311466846683136",
+				"departmentName":"市场部",
+				"procApplyTime":1569238151000,
+				"procCurrNodeUserId":"107163276022281011210742388013307043841074238801402007552",
+				"procCurrNodeUserName":"孙财 ",
+				"procData":"{\"start_time\":\"2019-09-01 00:00:00\",\"reason\":\"随便填\",\"processName\":\"加班\",\"end_time\":\"2019-09-02 00:00:00\",\"processKey\":\"process_overtime\",\"userId\":\"1066370498633486336\"}",
+				"processId":"1175975371407335424",
+				"processKey":"process_overtime",
+				"processName":"加班",
+				"processState":"2",
+				"timeOfEntry":1541318400000,
+				"userId":"1066370498633486336",
+				"username":"孙财"
+			},
+			{
+				"departmentId":"1175310929766055936",
+				"departmentName":"总裁办",
+				"procApplyTime":1583409279000,
+				"procCurrNodeUserId":"10637059899262279681075383133106425856107538313537135001612353967244972687361235396819959627776",
+				"procCurrNodeUserName":"管理员 乔海 董昊空 吕勇锐 袁永安 ",
+				"procData":"{\"start_time\":\"2020-03-17 00:00:00\",\"reason\":\"java开发\",\"processName\":\"离职\",\"end_time\":\"2020-03-29 00:00:00\",\"processKey\":\"process_dimission\",\"userId\":\"1063705989926227968\"}",
+				"processId":"1235413390924881920",
+				"processKey":"process_dimission",
+				"processName":"离职",
+				"processState":"1",
+				"timeOfEntry":1541145600000,
+				"userId":"1063705989926227968",
+				"username":"管理员"
+			},
+			{
+				"departmentId":"1175310929766055936",
+				"departmentName":"总裁办",
+				"procApplyTime":1583409371000,
+				"procCurrNodeUserId":"10637059899262279681075383133106425856107538313537135001612353967244972687361235396819959627776",
+				"procCurrNodeUserName":"管理员 乔海 董昊空 吕勇锐 袁永安 ",
+				"procData":"{\"reason\":\"个人原因\",\"processName\":\"离职\",\"exceptTime\":\"2020-03-25 00:00:00\",\"processKey\":\"process_dimission\",\"userId\":\"1063705989926227968\"}",
+				"processId":"1235413774988910592",
+				"processKey":"process_dimission",
+				"processName":"离职",
+				"processState":"1",
+				"timeOfEntry":1541145600000,
+				"userId":"1063705989926227968",
+				"username":"管理员"
+			},
+			{
+				"departmentId":"1175310929766055936",
+				"departmentName":"总裁办",
+				"procApplyTime":1583409412000,
+				"procCurrNodeUserId":"null",
+				"procCurrNodeUserName":"孙财 ",
+				"procData":"{\"start_time\":\"2020-03-01 00:00:00\",\"reason\":\"java工作计划\",\"processName\":\"加班\",\"end_time\":\"2020-03-25 00:00:00\",\"processKey\":\"process_overtime\",\"userId\":\"1063705989926227968\"}",
+				"processId":"1235413946837934080",
+				"processKey":"process_overtime",
+				"processName":"加班",
+				"processState":"1",
+				"timeOfEntry":1541145600000,
+				"userId":"1063705989926227968",
+				"username":"管理员"
+			}
+]
+</script>
+
+<style lang='scss'>
+.daBox{
+    height: 70px;
+    .dangBox{
+        height: 30px;
+        width: 270px;
+        background-color: #E6F7FF;
+        border: 1px solid #9FDAFF;
+    }
+    .el-button{
+        margin-left: 870px;
+    }
+}
+.daaBox{
+    margin-top: 20px;
+}
+.el-pagination{
+    margin-top: 20px;
+    margin-left: 1000px;
+}
+</style>
